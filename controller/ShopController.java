0a12a6c62b301acf3ddb940f4ba4536be82538e4
Yanice Slegers <yanice@dumbledore.local>
2016-09-21 09:51:25 +0200

    Begining of controller

--- controller/ShopController.java
+++ controller/ShopController.java
@@ -3,6 +3,10 @@ package controller;
 /**
  * Created by louis on 21/09/2016.
  */
+<<<<<<< Updated upstream:controller/ShopController.java
 public class Controller {
+=======
+public class Controller 
+>>>>>>> Stashed changes:controller/Controller.java
 
 }
