93e843c5456d9750ad1add47ba39d72bd54dc87b
KevinPeelmanR0485326 <kevin.peelman@student.ucll.be>
2016-10-10 11:17:28 +0200

    Merge

--- model/ProductRepo.java
+++ model/ProductRepo.java
@@ -7,7 +7,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.PrintWriter;	
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collection;
