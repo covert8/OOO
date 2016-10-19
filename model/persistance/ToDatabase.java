b973b8110d8506c331bdbfafff25962a0d55f135
covert8 <the.deller@gmail.com>
2016-10-19 08:58:20 +0200

    db load finished

--- model/persistance/ToDatabase.java
+++ model/persistance/ToDatabase.java
@@ -3,15 +3,17 @@ package model.persistance;
 import controller.ShopController;
 import model.client.Customer;
 import model.product.Product;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.HashMap;
 
 public class ToDatabase implements Persistable{
 
-
+	private static dbInterface db = new dbInterface();
 	@Override
 	public void init()
 	{
-
+		throw new NotImplementedException();
 	}
 
 	@Override
@@ -24,12 +26,18 @@ public class ToDatabase implements Persistable{
 	@Override
 	public HashMap<String, Product> loadProducts()
 	{
-		return null;
+		HashMap<String,Product> products = new HashMap<>();
+		dbInterface.getProducts().forEach(obj ->
+				products.put(obj.getProductId(), obj));
+		return products;
 	}
 
 	@Override
 	public HashMap<String, Customer> loadCustomers()
 	{
-		return null;
+		HashMap<String,Customer> customers = new HashMap<>();
+		dbInterface.getCustomers().forEach(obj ->
+				customers.put(obj.getName(),obj));
+		return customers;
 	}
 }
