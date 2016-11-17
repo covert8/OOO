52526a065e92a40174b33859f82f65b4c8e3b33d
Louis Roebben <louis.roebben@telenet.be>
2016-11-17 17:17:45 +0100

    lol

--- src/main/java/model/persistance/dbInterface.java
+++ src/main/java/model/persistance/dbInterface.java
@@ -1,21 +1,14 @@
 package model.persistance;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.LinkedList;
-
-import javax.swing.JOptionPane;
-
 import model.client.Customer;
 import model.product.CD;
 import model.product.Game;
 import model.product.Movie;
 import model.product.Product;
+
+import javax.swing.*;
+import java.sql.*;
+import java.util.LinkedList;
 
 final class dbInterface
 {
@@ -35,7 +28,7 @@ final class dbInterface
 
 	private static String driver = "org.apache.derby.jdbc.ClientDriver";
 	private static String connectionURL = "jdbc:derby:OOODB;create=true";
-	private static Connection connexion = null;
+	private static Connection connexion;
 
 	private static void loadDriver() {
 		try {
